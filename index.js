3769ed98c7c1d9cc6a3052680b038f9dbaa2be11
duminghong <i@duminghong.com>
2018-01-09 16:41:39 +0800

    修改index

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ var fs = require('fs');
 gifFrames({ url: 'gif/1.gif', frames: 'all' ,outputType: 'png', cumulative: true}).then(function (frameData) {
 	Promise.all(frameData.map(function(frame){
 		var data = frame.getImage();
-    	var index = frame.frameIndex;
-    	return new Promise(function (resolve, reject) {
-    		data.pipe(fs.createWriteStream('img/1/img' + index + '.png').on('finish', function() {
-			    resolve()
+		var index = frame.frameIndex;
+		return new Promise(function (resolve, reject) {
+			data.pipe(fs.createWriteStream('img/1/img' + index + '.png').on('finish', function() {
+				resolve()
 			}).on('error', function(err){
 				reject(err)
 			}));
-    	})
+		})
 	})).then(function(){
 		console.log('gif逐帧提取完成')
 		var proc = new ffmpeg({source: 'img/1/img%d.png'})
-	    	.videoCodec('mpeg4')
-	    	.withFps(10)
-	    	.on('end', function(stdout, stderr) {
-		  		console.log('视频生成完成');
+			.videoCodec('mpeg4')
+			.withFps(10)
+			.on('end', function(stdout, stderr) {
+				console.log('视频生成完成');
 			})
 			.on('error', function(err) {
-		        console.log('生成错误: ' + err.message)
-		    })
-	      	.saveToFile('video/1.mp4');
+				console.log('生成错误: ' + err.message)
+			})
+			.saveToFile('video/1.mp4');
 	})
 });
